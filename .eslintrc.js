9c746b4d19681cd1d8bd80563b6eeb89fbe01861
ImOverlord <9958853+ImOverlord@users.noreply.github.com>
2020-03-10 15:59:44 +0100

    Removed eslint from bundle (#7)
    Co-authored-by: ImOverlord <ImOverlord@hotmail.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,22 @@
 module.exports = {
-    root: true,
-    parser: '@typescript-eslint/parser',
-    plugins: [
-      '@typescript-eslint',
-    ],
-    extends: [
-      'eslint:recommended',
-      'plugin:@typescript-eslint/eslint-recommended',
-      'plugin:@typescript-eslint/recommended',
-    ],
-    rules: {
-        "@typescript-eslint/indent": ["error", 4, { "MemberExpression": 0 }],
-        "@typescript-eslint/no-explicit-any": 0,
-        "@typescript-eslint/interface-name-prefix": 0
-    }
-  };
+  ignorePatterns: [
+    "scripts/",
+    "docs/",
+    "dist/"
+  ],
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  rules: {
+    "@typescript-eslint/indent": ["error", 4, { "MemberExpression": 0 }],
+    "@typescript-eslint/no-explicit-any": 0,
+    "@typescript-eslint/interface-name-prefix": 0
+  }
+};
